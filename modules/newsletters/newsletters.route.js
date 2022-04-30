89d997baeabf5f308e927e7d2557de6baff718cf
Bhavik <iambhavik1999@gmail.com>
2022-04-30 13:34:05 +0530

    Added subscribe route to newsletter routes.

--- modules/newsletters/newsletters.route.js
+++ modules/newsletters/newsletters.route.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const newsletters = require("./newsletters.controller");
+const usersController = require("../users/users.controller");
 
 const { use, multerErrorHandler } = require("../../helpers/error-handler");
 const { upload } = require("../../helpers/file-upload");
@@ -10,5 +11,7 @@ router.post(
   use(newsletters.sendNewsLetter),
   multerErrorHandler
 );
+
+router.post("/subscribe", use(usersController.addUser));
 
 module.exports = router;
